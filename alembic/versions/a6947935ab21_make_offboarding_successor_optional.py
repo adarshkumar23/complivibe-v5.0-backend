6f4c21edb8b92d8669bdd0de8419dd469db46bad
Adarsh Kumar <adarsh23072005@gmail.com>
2026-07-05 18:23:02 +0000

    Repoint a6947935ab21 down_revision to 0198 after removing zombie 0199

--- alembic/versions/a6947935ab21_make_offboarding_successor_optional.py
+++ alembic/versions/a6947935ab21_make_offboarding_successor_optional.py
@@ -1,7 +1,7 @@
 """make offboarding successor optional
 
 Revision ID: a6947935ab21
-Revises: 0199_audit_engagement_source_schedule_link
+Revises: 0198_add_risk_assessment_to_issue_source_type
 Create Date: 2026-07-04 07:30:29.672629
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a6947935ab21'
-down_revision: Union[str, Sequence[str], None] = '0199_audit_engagement_source_schedule_link'
+down_revision: Union[str, Sequence[str], None] = '0198_add_risk_assessment_to_issue_source_type'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
